be2acbb945dfe46517c227dc2f1f5f48f56dff6a
Jemima <jemima.khoo98@gmail.com>
2022-05-08 21:46:45 +1000

    Added in product class

--- src/java/uts/isd/model/Product.java
+++ src/java/uts/isd/model/Product.java
@@ -9,14 +9,59 @@ package uts.isd.model;
  * @author Alice
  */
 public class Product {
+
     private String productName;
-    private String unitPrice;
+    private float unitPrice;
     private String productType;
-    private String quantity;
+    private int quantity;
     private String productDescription;
 
-    public Product() {
+    public Product(String productName, float unitPrice, String productType, int quantity, String productDescription) {
+        this.productName = productName;
+        this.unitPrice = unitPrice;
+        this.productType = productType;
+        this.quantity = quantity;
+        this.productDescription = productDescription;
+    }
 
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public float getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(float unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+
+    public String getProductType() {
+        return productType;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
     }
 
 }
