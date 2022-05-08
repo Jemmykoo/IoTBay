d3f9dd36275856d14877853c38c3a20ea3ec99f7
Jemima <jemima.khoo98@gmail.com>
2022-05-08 15:40:26 +1000

    Added in isstaff flag in users

--- build/web/createtables.sql
+++ build/web/createtables.sql
@@ -16,14 +16,16 @@ FIRSTNAME VARCHAR (255) NOT NULL,
 LASTNAME VARCHAR (255) NOT NULL,
 PASSWORD VARCHAR (255) NOT NULL,
 PHONE VARCHAR (255) NOT NULL,
+ISSTAFF BOOLEAN NOT NULL,
 PRIMARY KEY(ID)
 );
 
-INSERT INTO USERS(EMAIL,FIRSTNAME,LASTNAME,PASSWORD,PHONE)
+INSERT INTO USERS(EMAIL,FIRSTNAME,LASTNAME,PASSWORD,PHONE,ISSTAFF)
 VALUES
-('harry.potter@student.uts.edu.au','Harry','Potter','stag','0412345678'),
-('ron.weasley@student.uts.edu.au','Ron','Weasley','dog','0412666666'),
-('hermione.granger@student.uts.edu.au','Hermione','Granger','otter','0412333444');
+('harry.potter@student.uts.edu.au','Harry','Potter','stag','0412345678',FALSE),
+('ron.weasley@student.uts.edu.au','Ron','Weasley','dog','0412666666',FALSE),
+('hermione.granger@student.uts.edu.au','Hermione','Granger','otter','0412333444',FALSE),
+('albus.dumbledore@uts.edu.au','Albus','Dumbledore','phoenix','1234567890',TRUE);
 
 
 SELECT * FROM USERS;
