7d1df0aad4b2c31cba2ba3f8b06f6f12fc65af76
Jemima <jemima.khoo98@gmail.com>
2022-05-13 22:13:24 +1000

    Not tested, maybe not working, added in serverlet

---
+++ src/java/uts/isd/controller/ProductsServerlet.java
@@ -0,0 +1,152 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+/*package uts.isd.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uts.isd.controller;
+
+import java.io.IOException;
+
+import java.sql.Connection;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import uts.isd.model.dao.*;
+
+public class ProductsServerlet extends HttpServlet {
+
+    private DBConnector db;
+    private DBProductManager ProductManager;
+    private Connection conn;
+
+    @Override //Create and instance of DBConnector for the deployment session
+    public void init() {
+        try {
+            db = new DBConnector();
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(ProductsServerlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @Override //Add the DBConnector, DBManager, Connection instances to the session
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        HttpSession session = request.getSession();
+        conn = db.openConnection();
+        try {
+            ProductManager = new DBProductManager(conn);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductsServerlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        //export the DB manager to the view-session (JSPs)
+        session.setAttribute("ProductManager", ProductManager);
+    }
+
+    @Override //Destroy the servlet and release the resources of the application (terminate also the db connection)
+
+    public void destroy() {
+        try {
+            db.closeConnection();
+        } catch (SQLException ex) {
+            Logger.getLogger(ProductsServerlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
+
+
+
+
+
+
+/**
+ *
+ * @author Alice
+ */
+public class ProductsServerlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet ProductsServerlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet ProductsServerlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
